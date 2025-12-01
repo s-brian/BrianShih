415e550102e5bdcac5039759e45aa259af8ff931
Brian Shih <shih.brian123@gmail.com>
2025-12-01 16:36:42 +0800

    v4_石邦瑞

--- app/src/main/java/tw/edu/pu/csim/tcyang/brianshih/ExamScreen.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/brianshih/ExamScreen.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.csim.tcyang.brianshih
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,28 +11,32 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+
+private val IMAGE_SIZE_DP = 100.dp
 
 @Composable
 fun ExamScreen(examViewModel: ExamViewModel = viewModel()){
     val width = examViewModel.screenWidth
     val height = examViewModel.screenHeight
 
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Yellow)
     ) {
-
 
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -63,6 +68,7 @@ fun ExamScreen(examViewModel: ExamViewModel = viewModel()){
                 color = Color.Black,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
+
             Text(
                 text = "Author: Brian Shih (石邦瑞) Computer Science Department",
                 color = Color.Black,
@@ -91,6 +97,27 @@ fun ExamScreen(examViewModel: ExamViewModel = viewModel()){
             Image(
                 painter = painterResource(id = R.drawable.role3),
                 contentDescription = "Bottom Right Image",
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .size(IMAGE_SIZE_DP)
+                .offset(x = examViewModel.circleX.dp, y = examViewModel.circleY.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        val dragAmountDpX = with(density) { dragAmount.x.toDp().value }
+                        val dragAmountDpY = with(density) { dragAmount.y.toDp().value }
+
+                        examViewModel.MoveCircle(dragAmountDpX)
+                    }
+                }
+        ) {
+            Image(
+                painter = painterResource(id = examViewModel.selectedImageId),
+                contentDescription = "Randomly selected item",
+                modifier = Modifier.fillMaxSize()
             )
         }
     }
--- app/src/main/java/tw/edu/pu/csim/tcyang/brianshih/ExamViewModel.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/brianshih/ExamViewModel.kt
@@ -3,19 +3,69 @@ package tw.edu.pu.csim.tcyang.brianshih
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class ExamViewModel : ViewModel() {
+    private val imageList = listOf(
+        R.drawable.service0,
+        R.drawable.service1,
+        R.drawable.service2,
+        R.drawable.service3
+    )
+    var circleX by mutableStateOf(0f)
+    var circleY by mutableStateOf(0f)
     var screenWidth by mutableStateOf(0.0f)
         private set
-
     var screenHeight by mutableStateOf(0.0f)
         private set
+
+    var selectedImageId by mutableStateOf(R.drawable.service0)
+        private set
+
 
     fun SetGameSize(width: Float, height: Float) {
         screenWidth = width
         screenHeight = height
+
+        circleX = 130f
+        circleY = 0f
+
+        pickRandomImage()
+        viewModelScope.launch {
+            startFalling()
+        }
     }
 
+    fun MoveCircle(dx: Float) {
+        circleX += dx
 
+    }
+
+    fun pickRandomImage() {
+        selectedImageId = imageList[Random.nextInt(imageList.size)]
+    }
+    private val DROP_DISTANCE_DP = 20f
+    private val DROP_INTERVAL_MS = 100L
+    private fun resetImagePosition() {
+        circleX = 130f
+        circleY = 0f
+    }
+
+    suspend fun startFalling() {
+        while (true) {
+            delay(DROP_INTERVAL_MS)
+
+            circleY += DROP_DISTANCE_DP
+
+            if (circleY >= screenWidth - 300) {
+                resetImagePosition()
+                pickRandomImage()
+            }
+        }
+    }
 }
